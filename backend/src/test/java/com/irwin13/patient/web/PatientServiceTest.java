29e5b2f64108880ca238395f7c1a5c1df5cddc8d
Muhammad Irwin <irwin13@gmail.com>
2024-01-30 23:39:10 +0700

    add mock service test

---
+++ backend/src/test/java/com/irwin13/patient/web/PatientServiceTest.java
@@ -0,0 +1,52 @@
+package com.irwin13.patient.web;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.irwin13.patient.web.controller.PatientController;
+import com.irwin13.patient.web.entity.Patient;
+import com.irwin13.patient.web.service.PatientService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@WebMvcTest(PatientController.class)
+public class PatientServiceTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    PatientService patientService;
+
+    @Test
+    void shouldReturnAllPatient() throws Exception {
+        List<Patient> list = new LinkedList<>();
+
+        Patient patient1 = new Patient();
+        patient1.setFirstName("First");
+        list.add(patient1);
+
+        Patient patient2 = new Patient();
+        patient2.setFirstName("Second");
+        list.add(patient2);
+
+        Patient patient3 = new Patient();
+        patient3.setFirstName("Third");
+        list.add(patient3);
+
+        when(patientService.search()).thenReturn(list);
+
+        this.mockMvc.perform(get("/patient/search"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)));
+
+    }
+}
