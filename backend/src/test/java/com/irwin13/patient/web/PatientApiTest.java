c39b47d2bf4d03aa4426e56aa95bcfa62a2d9059
Muhammad Irwin <irwin13@gmail.com>
2024-01-30 23:39:03 +0700

    add mock service test

--- backend/src/test/java/com/irwin13/patient/web/PatientApiTest.java
+++ backend/src/test/java/com/irwin13/patient/web/PatientApiTest.java
@@ -1,13 +1,13 @@
 package com.irwin13.patient.web;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.irwin13.patient.web.entity.Patient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PatientApiTest {
