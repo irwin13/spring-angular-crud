3cc608d1ce5633ecd9236dc2cbda1ddf05e122fe
Muhammad Irwin <irwin13@gmail.com>
2024-02-05 09:15:09 +0700

    add percent on findByName

--- backend/src/main/java/com/irwin13/patient/web/service/PatientService.java
+++ backend/src/main/java/com/irwin13/patient/web/service/PatientService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -43,6 +44,9 @@ public class PatientService {
     }
 
     public List<Patient> findByName(String name) {
+        if (!StringUtils.isEmpty(name)) {
+            name = "%" + name + "%";
+        }
         return patientRepository.findByName(name);
     }
 
